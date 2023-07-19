4dd23fb61167fd8e295c27fddaf563308fa07193
light-david <132290741+light-david@users.noreply.github.com>
2023-07-19 22:00:39 +0100

    Create Boolean_values_to_string.py
    Complete the method that takes a boolean value and returns a "Yes" for true or a "No" for false.

---
+++ Boolean_values_to_string.py
@@ -0,0 +1,8 @@
+#Defines bool_to_word function which takes one argument
+def bool_to_word(boolean):
+    #Checks if boolean is True
+    if boolean == True:
+        #Returns Yes
+        return "Yes"
+    #Returns No
+    return "No"
