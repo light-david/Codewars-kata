30db27eb902641118dd5ba256db8b0c72a8cff32
light-david <132290741+light-david@users.noreply.github.com>
2023-07-20 16:09:37 +0100

    Keep_Hydrated.py
    Nathan loves cycling.
    
    Because Nathan knows it is important to stay hydrated, he drinks 0.5 liters of water per hour of cycling.
    
    You get given the time in hours and you need to return the number of liters Nathan will drink, rounded to the smallest value.

---
+++ Keep_Hydrated.py
@@ -0,0 +1,9 @@
+#Imports math moudle
+import math
+
+#Defines the litres fucntion
+def litres(time):
+    #Rounds down litre
+    litre = math.floor(time * 0.5)
+    #Returns litre
+    return litre
