e8add862c9c75daaeb493068c7fe3d178aba60fd
light-david <132290741+light-david@users.noreply.github.com>
2023-07-21 07:55:41 +0100

    Area_to_perimeter.py
    You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
    If it is a square, return its area. If it is a rectangle, return its perimeter.

---
+++ Area_to_perimeter.py
@@ -0,0 +1,6 @@
+#Defines the area_or_perimeter function which takes two arguments
+def area_or_perimeter(l , w):
+    #Checks the vaules of l and w
+    if l == w:
+        return l * w
+    return (l + w) * 2
