7ee6ddcac2e996a54541d4cffae5a213894d0c14
David <132290741+light-david@users.noreply.github.com>
2023-07-30 17:36:46 +0100

    Update String_end.py
    Added an alternative function without having to use a built-in function.

--- String_end.py
+++ String_end.py
@@ -2,3 +2,12 @@
 def solution(text, ending):
     #Returns True if ending is in text
     return text.endswith(ending)
+
+##""""""""Alternative function to solution""""""""
+
+#Defines solution method and takes two arguments
+def solution(text, ending):
+    #Checks if the last elements of text is ending
+    if text[-len(ending):] == ending:
+        return True
+    return False
