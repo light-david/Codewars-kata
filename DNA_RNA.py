8aa4992143fa43b374e978ae72b6272af4d15651
David <132290741+light-david@users.noreply.github.com>
2023-08-03 22:29:26 +0100

    DNA_RNA.py
    This function converts DNA to RNA. It replaces the "T" in DNA to "U" in RNA.

---
+++ DNA_RNA.py
@@ -0,0 +1,4 @@
+#Defines a function called dna_to_rna and takes one argument
+def dna_to_rna(dna):
+    #Replaces "T" in the taken argument with "U" and returns the argument
+    return dna.replace("T","U")
