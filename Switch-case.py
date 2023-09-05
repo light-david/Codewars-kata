44cbc33138158c1b42f1926b496984f76e47f33c
David <132290741+light-david@users.noreply.github.com>
2023-09-05 15:16:56 +0100

    Switch-case.py
    This function takes one argument and uses match case to check if the argument is vaild.

---
+++ Switch-case.py
@@ -0,0 +1,25 @@
+#Defines a function and takes one parameter
+def switch_it_up(number):
+    #Matches the given argument with each case
+    match number:
+        case 0:
+            return "Zero"
+        case 1:
+            return "One"
+        case 2:
+            return "Two"
+        case 3:
+            return "Three"
+        case 4:
+            return "Four"
+        case 5:
+            return "Five"
+        case 6:
+            return "Six"
+        case 7:
+            return "Seven"
+        case 8:
+            return "Eight"
+        case 9:
+            return "Nine"
+    return "Invalid Input"
